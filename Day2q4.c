075d31fe2fdbd2ac30669fa3819d49e112a5c4d3
Sanidhya Bhardwaj <sanidhyabhardwaj300@gmail.com>
2026-09-09 16:44:37 +0530

    submit

--- Day2q4.c
+++ Day2q4.c
@@ -4,7 +4,7 @@ int main() {
     float radius;
     scanf("%f", &radius);
     float area = 3.14 * radius * radius;
-    float circumference = 2 * 3.14 * radius;
-    printf("Area=%.2f, Circumference=%.2f", area, circumference);
+    
+    printf("Area=%.2f, ", area);
     return 0;
-}
+}  
